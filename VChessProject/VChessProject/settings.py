e3c3530cf49f6f98fb23707b66bcdba5c4092051
Воронин Владимир <vladimirvoron7@gmail.com>
2024-05-21 15:10:32 +0300

    Add connection to PostgreSQL DB

--- VChessProject/VChessProject/settings.py
+++ VChessProject/VChessProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from . import hidden
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'VChessProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'VChessDB',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': hidden.VCHESSDB_PASSWORD,
+        'PORT': 5434
     }
 }
 
