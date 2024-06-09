610de99f4109733eda5b696d2bc0b551d9cc17c9
Воронин Владимир <vladimirvoron7@gmail.com>
2024-06-09 15:20:22 +0300

    add search new game, add/delete player from q logic with celery tasks

--- VChessProject/chess/urls.py
+++ VChessProject/chess/urls.py
@@ -7,5 +7,13 @@ urlpatterns = [
     path("signup", views.SignUpView.as_view(), name="sign_up"),
     path("login", views.LogInView.as_view(), name="log-in"),
     path("logout", views.logout_view, name="log-out"),
-    path("board", views.BoardView.as_view(), name="board"),
+    path("board", views.BoardView.as_view(), name="board")
 ]
+
+ajaxpatterns = [
+    path("ajax_start_search", views.ajax_start_search, name="ajax_start_search"),
+    path("ajax_cancel_search", views.ajax_cancel_search, name="ajax_cancel_search"),
+    # path("get_ajax", views.get_ajax, name="get_ajax")
+]
+
+urlpatterns += ajaxpatterns
--- VChessProject/chess/views.py
+++ VChessProject/chess/views.py
@@ -1,14 +1,21 @@
+import json
+import time
 from pathlib import Path
 
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 from django.views import View
 from django.views.generic import TemplateView, FormView
 
+from . import config
 from .forms import SignUpForm, LoginForm
 import logging
+
+from .tasks import add_player_to_search_queue, delete_player_from_search_queue, start_global_search, \
+    PlayerSearchTaskRedis
 
 logger = logging.getLogger(__name__)
 
@@ -61,3 +68,34 @@ def logout_view(request):
 
 class BoardView(TemplateView):
     template_name = Path("chess/board.html")
+
+
+def simulation():
+    for i in range(2, 7):
+        add_player_to_search_queue.delay(i, 180, None, 1200)
+
+
+def clear_redis():
+    a = PlayerSearchTaskRedis()
+    a.redis_clear()
+
+
+def ajax_start_search(request):
+    # clear_redis()
+    logger.info("Start search")
+    start_global_search.delay()
+    data = json.loads(tuple(request.GET)[0])
+    user = User.objects.get(username=request.user)
+    r = add_player_to_search_queue.delay(user.id, data["full_time"], data["additional_time"], 1200)
+    return JsonResponse({"How": "Long"})
+
+
+def ajax_cancel_search(request):
+    logger.info("Cancel search")
+    user = User.objects.get(username=request.user)
+    delete_player_from_search_queue.delay(user.id)
+    return JsonResponse({"Cancel": "Search"})
+
+
+def ajax_get_match_if_found(request):
+    logger.info("Getting match")
