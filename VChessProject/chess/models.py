a602dd81346118882920cfa7182621647c0b9dac
Воронин Владимир <vladimirvoron7@gmail.com>
2024-06-15 13:29:04 +0300

    FideTitle model and User one to one update logic

--- VChessProject/chess/models.py
+++ VChessProject/chess/models.py
@@ -3,6 +3,17 @@ import random
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class FideTitle(models.Model):
+    short_name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        unique_together = ('short_name', 'name',)
 
 
 class ChessUser(models.Model):
@@ -13,13 +24,15 @@ class ChessUser(models.Model):
     classic_rating = models.FloatField(null=True)
 
 
-class FideTitle(models.Model):
-    short_name = models.CharField(max_length=50)
-    name = models.CharField(max_length=50)
-    description = models.TextField(blank=True, null=True)
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        ChessUser.objects.create(user=instance)
 
-    class Meta:
-        unique_together = ('short_name', 'name',)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.chessuser.save()
 
 
 class GameType(models.Model):
